aea5c9d634d3dbf5034372633945b4d737748b77
William Ward <46961871+WilliamWard074@users.noreply.github.com>
2020-02-27 21:24:57 +0000

    Tweak styling

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -14,17 +14,17 @@
 
             <!-- Task Name -->
             <div class="form-group">
-                <label for="task-name" class="col-sm-3 control-label">Task</label>
+                <label for="task-name" class="control-label">Task</label>
 
-                <div class="col-sm-6">
+                <div class="">
                     <input type="text" name="name" id="task-name" class="form-control">
                 </div>
             </div>
 
             <!-- Add Task Button -->
             <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
+                <div class="col-sm-offset-3">
+                    <button type="submit" class="btn btn-primary">
                         <i class="fa fa-plus"></i> Add Task
                     </button>
                 </div>
@@ -63,7 +63,7 @@
                                         {{ csrf_field() }}
                                         {{ method_field('DELETE') }}
 
-                                        <button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger">
+                                        <button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger float-right">
                                             <i class="fa fa-btn fa-trash"></i>Delete
                                         </button>
                                     </form>
