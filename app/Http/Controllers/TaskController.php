4b372bb123a541d096b3299396049006b2301e83
William Ward <46961871+WilliamWard074@users.noreply.github.com>
2020-02-27 21:07:12 +0000

    Create task controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
+
+class TaskController extends Controller
+{
+	
+}
