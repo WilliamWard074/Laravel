ea01275bb1aeea99c15d8eba3bd12676f7a242f5
William Ward <46961871+WilliamWard074@users.noreply.github.com>
2020-02-27 21:08:46 +0000

    Inject tasks repository

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,4 +11,23 @@ use App\Repositories\TaskRepository;
 class TaskController extends Controller
 {
 	
+	/**
+     * The task repository instance.
+     *
+     * @var TaskRepository
+     */
+    protected $tasks;
+    
+    /**
+     * Create a new controller instance.
+     *
+     * @param  TaskRepository  $tasks
+     * @return void
+     */
+    public function __construct(TaskRepository $tasks)
+    {
+        $this->middleware('auth');
+
+        $this->tasks = $tasks;
+    }
 }
